412e879a49bcfb583fd9c946324b672709f6a4ff
madi <rahmadi.rr@gmail.com>
2022-03-27 19:12:01 +0700

    server: ready to deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: ['localhost', 'naflibox.herokuapp.com'],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  basePath: '',
 };
